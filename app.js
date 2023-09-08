087aeddb3d90701f0b4eda94aff28fc3d17fb372
Alice <johndoe@example.com>
2023-09-08 16:28:42 +0200

    first try(test)

---
+++ app.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+// require('dotenv/config');
+
+// ℹ️ Connects to the database
+// require('./db');
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+
+
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+
+
+const express = require('express');
+
+const hbs = require('hbs');
+const path = require('path');
+
+const app = express();
+
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, resp) => {
+    resp.render("index");
+  });
+
+// app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// require('./config/session.config')(app);
+// require('./config')(app);
+
+// const projectName = 'lab-express-basic-auth';
+// const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+
+// app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+
+// const index = require('./routes/index');
+// app.use('/', index);
+
+
+// require('./error-handling')(app);
+
+module.exports = app;
