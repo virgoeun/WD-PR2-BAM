ac6d7c6ff185b0e6acff4e857379435d653bb145
Alice <johndoe@example.com>
2023-09-11 11:28:35 +0200

    user model updated

--- model/user.model.js
+++ model/user.model.js
@@ -1,18 +1,45 @@
+
+
+const bcrypt = require("bcryptjs");
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
-  username: {
-    type: String,
-    unique: true,
-    required: [true, 'Username is required.']
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Username is required."],
+      unique: [true, "Username is already taken."],
+      trim: true,
+      match: [
+        /^[A-Za-z][A-Za-z0-9_]{5,21}$/,
+        "Your username should be at least 6 characters long, start with a letter, and can include letters, numbers, or underscores.",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.'],
+      match: [
+        /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+      ],
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png",
+    },
+ 
+    fullName: {
+      type: String,
+    },
+posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
   },
-  password: String
-},
-{
-  timestamps: true
-}
-);
+  {
+    timestamps: true,
+  }
+
+)
 
 const User = model("User", userSchema);
 
