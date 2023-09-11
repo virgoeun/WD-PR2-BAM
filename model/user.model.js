82201c9b628a91c43a824d62c7f3840132901bb6
Madhushree B <boylemadhushree@gmail.com>
2023-09-11 09:09:01 +0200

    add model file

---
+++ model/user.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+  username: {
+    type: String,
+    unique: true,
+    required: [true, 'Username is required.']
+  },
+  password: String
+},
+{
+  timestamps: true
+}
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
