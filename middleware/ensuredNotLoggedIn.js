55fb82788562df222e7695acfe081a665e19ce7d
Alice <johndoe@example.com>
2023-09-13 11:43:37 +0200

    journal fix

---
+++ middleware/ensuredNotLoggedIn.js
@@ -0,0 +1,11 @@
+// Middleware to protect login and signup routes from logged-in users
+function ensureNotLoggedIn(req, res, next) {
+    if (req.session.currentUser) {
+      // User is already logged in, redirect to their profile page
+      return res.redirect("/userProfile");
+    }
+    // User is not logged in, proceed to the login/signup route
+    next();
+  }
+  
+  module.exports = {ensureNotLoggedIn}
